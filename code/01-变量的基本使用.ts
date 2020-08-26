2ef2fbd3e00e78440bd6dac318c6cea0a51822b4
wuli-erqio <1766458200@qq.com>
2020-08-26 16:24:48 +0800

    变量的基本使用

---
+++ code/01-变量的基本使用.ts
@@ -0,0 +1,14 @@
+// 第一步：声明变量在指定类型
+// let age: number
+// 第二步：给变量赋值
+// age = 18
+// console.log(age)
+
+// 变量是可变化的
+// age = 19
+// console.log(age)
+
+
+// 简化方法: 声明变量的同时就赋值
+let age: number = 18
+console.log(age)
