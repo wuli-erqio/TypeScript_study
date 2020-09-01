ea0aaada7f3b4f31a92a77705414ebced86b5ccc
wuli-erqio <1766458200@qq.com>
2020-09-01 10:53:06 +0800

    ts 函数返回值

---
+++ code/05_函数的返回值.ts
@@ -0,0 +1,10 @@
+function getSum(nums: number[]): number {
+  let sum: number = 0
+  for(let i: number = 0; i < nums.length; i++) {
+    sum += nums[i]
+  }
+  // console.log(sum)
+  return sum
+}
+let number: number = getSum([1,3,5]) + getSum([2,4,6])
+console.log(number)
