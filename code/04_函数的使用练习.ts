38e247cb3d02f70310250cc68498bbdcfe845aed
wuli-erqio <1766458200@qq.com>
2020-09-01 11:42:28 +0800

    变量的作用域

--- code/04_函数的使用练习.ts
+++ code/04_函数的使用练习.ts
@@ -2,13 +2,13 @@
 // 计算两个标量num1、num2的和
 
 // 无参函数，函数体计算的数值没办法改变
-function sum() {
-  let num1:number = 10
+function sum2() {
+  let num1: number = 10
   let num2: number = 20
   let result: number = num1 + num2
   console.log(result)
 }
-sum()
+sum2()
 
 // 有参数，每次调用可以传入不同的数据
 function sum1(num1: number = 0, num2: number = 0) {
@@ -18,11 +18,11 @@ function sum1(num1: number = 0, num2: number = 0) {
 sum1(50, 20)
 
 
-function getSum(nums: number[]) {
+function getSum1(nums: number[]) {
   let sum: number = 0
   for(let i: number = 0; i < nums.length; i++) {
     sum += nums[i]
   }
   console.log(sum)
 }
-getSum([1,3,4])
+getSum1([1,3,4])
--- code/05_函数的返回值.ts
+++ code/05_函数的返回值.ts
@@ -1,4 +1,4 @@
-function getSum(nums: number[]): number {
+function getSum (nums: number[]): number {
   let sum: number = 0
   for(let i: number = 0; i < nums.length; i++) {
     sum += nums[i]
