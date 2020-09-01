964a19d122c2bdcb913e7d98ff8e17b538210a98
wuli-erqio <1766458200@qq.com>
2020-09-01 10:16:19 +0800

    ts 函数参数练习

--- code/04_函数的使用练习.ts
+++ code/04_函数的使用练习.ts
@@ -11,8 +11,18 @@ function sum() {
 sum()
 
 // 有参数，每次调用可以传入不同的数据
-function sum1(num1:number = 0, num2: number = 0) {
+function sum1(num1: number = 0, num2: number = 0) {
   let result: number = num1 + num2
   console.log(result)
 }
 sum1(50, 20)
+
+
+function getSum(nums: number[]) {
+  let sum: number = 0
+  for(let i: number = 0; i < nums.length; i++) {
+    sum += nums[i]
+  }
+  console.log(sum)
+}
+getSum([1,3,4])
